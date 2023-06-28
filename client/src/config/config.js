c9d0adc7b10f188595bb5bbfa7aa495cab094d02
sickboi <nikita.tsepochko20@gmail.com>
2023-06-28 17:20:22 +0300

    Initial commit

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "https://ai-threejs-project-wqvt.onrender.com/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
